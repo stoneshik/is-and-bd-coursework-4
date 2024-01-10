f8605140c916ac960af9cd0c990d0cac1915aca8
stoneshik <strebilly@mail.ru>
2024-01-10 19:45:21 +0300

    прописал доступные страницы

--- src/main/java/com/main/controller/RouterController.java
+++ src/main/java/com/main/controller/RouterController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RouterController {
+    /* Страницы для неавторизованного пользователя */
     @GetMapping("/")
     public String getHomeWithoutLoginPage() {
         return "index";
@@ -21,9 +22,30 @@ public class RouterController {
     public String getMapWithoutLoginPage() {
         return "index";
     }
-
+    /* Страницы для авторизованного пользователя */
     @GetMapping("/main")
     public String getHomePage() {
         return "index";
     }
+    @GetMapping("/map")
+    public String getMapPage() {
+        return "index";
+    }
+    @GetMapping("/new_order_print")
+    public String getNewOrderPrintPage() {
+        return "index";
+    }
+    @GetMapping("/new_order_scan")
+    public String getNewOrderScanPage() {
+        return "index";
+    }
+    @GetMapping("/cart")
+    public String getCartPage() {
+        return "index";
+    }
+    @GetMapping("/files")
+    public String getFilesPage() {
+        return "index";
+    }
+
 }
