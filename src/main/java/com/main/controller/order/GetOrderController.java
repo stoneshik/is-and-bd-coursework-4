e71b8975c2317ad01a59edfef12a4eb83b4c1aeb
stoneshik <strebilly@mail.ru>
2024-01-13 20:58:07 +0300

    небольшие правки

--- src/main/java/com/main/controller/order/GetOrderController.java
+++ src/main/java/com/main/controller/order/GetOrderController.java
@@ -1,6 +1,7 @@
-package com.main.controller;
+package com.main.controller.order;
 
 import com.main.ResponseMessageWrapper;
+import com.main.entities.order.OrderStatus;
 import com.main.entities.order.OrderWithAddress;
 import com.main.security.AuthorizeHandler;
 import com.main.services.OrderWithAddressService;
@@ -20,11 +21,17 @@ public class GetOrderController {
     private final OrderWithAddressService orderWithAddressService;
     private final AuthorizeHandler authorizeHandler;
 
-    @GetMapping(
-            path = "/api/order/get_paid",
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<Object> getPaidOrders(HttpServletRequest httpServletRequest) {
+    private List<OrderWithAddress> getOrdersFromBd(OrderStatus orderStatus, String login) {
+        List<OrderWithAddress> orders;
+        switch (orderStatus) {
+            case PAID -> orders = orderWithAddressService.getPaidOrders(login);
+            case NOT_PAID -> orders = orderWithAddressService.getNotPaidOrders(login);
+            default -> orders = null;
+        }
+        return orders;
+    }
+
+    private ResponseEntity<Object> getOrders(HttpServletRequest httpServletRequest, OrderStatus orderStatus) {
         final String login = authorizeHandler.getLoginBySessionId(httpServletRequest);
         if (login.isEmpty()) {
             return new ResponseEntity<>(
@@ -32,7 +39,7 @@ public class GetOrderController {
                     HttpStatus.BAD_REQUEST
             );
         }
-        final List<OrderWithAddress> orders = orderWithAddressService.getPaidOrders(login);
+        final List<OrderWithAddress> orders = getOrdersFromBd(orderStatus, login);
         if (orders == null) {
             return new ResponseEntity<>(
                     new ResponseMessageWrapper("Не получилось получить информацию о заказах"),
@@ -43,24 +50,18 @@ public class GetOrderController {
     }
 
     @GetMapping(
+            path = "/api/order/get_paid",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Object> getPaidOrders(HttpServletRequest httpServletRequest) {
+        return getOrders(httpServletRequest, OrderStatus.PAID);
+    }
+
+    @GetMapping(
             path = "/api/order/get_not_paid",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<Object> getNotPaidOrders(HttpServletRequest httpServletRequest) {
-        final String login = authorizeHandler.getLoginBySessionId(httpServletRequest);
-        if (login.isEmpty()) {
-            return new ResponseEntity<>(
-                    new ResponseMessageWrapper("Пользователь не авторизован"),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-        final List<OrderWithAddress> orders = orderWithAddressService.getNotPaidOrders(login);
-        if (orders == null) {
-            return new ResponseEntity<>(
-                    new ResponseMessageWrapper("Не получилось получить информацию о заказах"),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-        return new ResponseEntity<>(orders, HttpStatus.OK);
+        return getOrders(httpServletRequest, OrderStatus.NOT_PAID);
     }
 }
