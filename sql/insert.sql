b310265901e66828121fb20a884d0cbc2428d4c2
stoneshik <strebilly@mail.ru>
2024-01-30 21:22:46 +0300

    правки в создание нового заказа на печать

--- sql/insert.sql
+++ sql/insert.sql
@@ -45,15 +45,25 @@ INSERT INTO vending_points(
     vending_point_cords)
 VALUES
     (0,
+     'Невский проспект, 1/4',
+     'Находится рядом с банкоматом',
+     2,
+     '{59.936846, 30.312185}'),
+    (1,
      'Невский пр-кт, 2',
      'Находится рядом с банкоматом',
      2,
-     '{59.93716299256363, 30.313378213455664}'),
-    (1,
+     '{59.937594, 30.313631}'),
+    (2,
+     'Кронверкский проспект, 21/2',
+     'Находится во дворе',
+     2,
+     '{59.956940, 30.319282}'),
+    (3,
      'Комендантcкий пр-кт, 34',
      'Находится в подвале рядом с шаурмечной',
      1,
-     '{60.02097852901829, 30.242859111340625}')
+     '{60.021227, 30.243383}')
     ;
 INSERT INTO vending_point_schedules(
     vending_point_schedule_id,
@@ -62,13 +72,23 @@ INSERT INTO vending_point_schedules(
     vending_point_schedule_time_start,
     vending_point_schedule_time_end)
 VALUES
-    (0, 0, 'monday', '10:00', '20:00'),
-    (1, 0, 'tuesday', '09:00', '21:00'),
-    (2, 0, 'wednesday', '11:00', '23:00'),
-    (3, 0, 'thursday', '10:00', '20:00'),
-    (4, 0, 'friday', '10:00', '20:00'),
-    (5, 1, 'saturday', '06:35', '22:20'),
-    (6, 1, 'sunday', '09:03', '21:55')
+    (default, 0, 'monday', '10:00', '20:00'),
+    (default, 0, 'tuesday', '09:00', '21:00'),
+    (default, 0, 'wednesday', '11:00', '23:00'),
+    (default, 0, 'thursday', '10:00', '20:00'),
+    (default, 0, 'friday', '10:00', '20:00'),
+    (default, 1, 'saturday', '06:35', '22:20'),
+    (default, 1, 'sunday', '09:03', '21:55'),
+    (default, 2, 'monday', '10:00', '20:00'),
+    (default, 2, 'tuesday', '09:00', '21:00'),
+    (default, 2, 'wednesday', '11:00', '23:00'),
+    (default, 2, 'thursday', '10:00', '20:00'),
+    (default, 2, 'friday', '10:00', '20:00'),
+    (default, 3, 'monday', '10:00', '20:00'),
+    (default, 3, 'tuesday', '09:00', '21:00'),
+    (default, 3, 'wednesday', '11:00', '23:00'),
+    (default, 3, 'thursday', '10:00', '20:00'),
+    (default, 3, 'friday', '10:00', '20:00')
     ;
 INSERT INTO vending_point_unusual_schedules(
     vending_point_unusual_schedule_id,
@@ -77,14 +97,15 @@ INSERT INTO vending_point_unusual_schedules(
     vending_point_schedule_time_start,
     vending_point_schedule_time_end)
 VALUES
-    (0, 0, '15.11.2024', '02:00', '22:00')
+    (default, 0, '15.11.2024', '02:00', '22:00')
     ;
 -- Вендинговые аппараты (машины)
 INSERT INTO machines(machine_id, vending_point_id)
 VALUES
     (0, 0),
     (1, 0),
-    (2, 1)
+    (2, 1),
+    (3, 2)
     ;
 INSERT INTO function_variants(function_variant_id, vending_point_id, machine_id, function_variant)
 VALUES
@@ -94,7 +115,10 @@ VALUES
     (default, 0, 1, 'color_print'),
     (default, 0, 1, 'scan'),
     (default, 1, 2, 'black_white_print'),
-    (default, 1, 2, 'scan')
+    (default, 1, 2, 'scan'),
+    (default, 2, 2, 'black_white_print'),
+    (default, 3, 3, 'black_white_print'),
+    (default, 3, 3, 'scan')
     ;
 INSERT INTO machine_supplies(
     machine_supplies_id,
